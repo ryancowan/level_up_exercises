88e9bdb8ce8e1dd50b9fa00a6ff1ff4e833e5405
rcowan <ryancowan@gmail.com>
2015-03-23 11:34:55 -0500

    turn all_related method into a scope

--- opposite_of_progress/congress/app/models/deed.rb
+++ opposite_of_progress/congress/app/models/deed.rb
@@ -1,9 +1,5 @@
 class Deed < ActiveRecord::Base
   scope :all_sorted, ->(page, sort_by = "occurrence_date DESC") { order(sort_by).paginate(page: page) }
   scope :related_bill, ->(deed) { where(bioguide_id: deed.bioguide_id).first }
-
-  def self.all_related(deed)
-    Deed.where('id != ? and (bioguide_id = ? or bill_id = ?)',
-               deed.id, deed.bioguide_id, deed.bill_id)
-  end
+  scope :all_related, ->(deed) { where('id != ? and (bioguide_id = ? or bill_id = ?)', deed.id, deed.bioguide_id, deed.bill_id) }
 end
